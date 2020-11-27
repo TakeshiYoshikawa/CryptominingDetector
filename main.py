9816a3d99a683e4165a42a2432c482c94659c48d
Arthur Takeshi <takeshi-y@hotmail.com>
2020-11-27 15:26:44 -0400

    firewall alert shows detected ips

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-import src.stratum.stratum
-
-def run():
-    # filename = sniff(count=1000, filter="tcp")
-    # wrpcap("pkt/miner.pcap", filename)
-    print(find_stratum("pkt/miner.pcap"))
-
-    # send_firewall_alert("8.8.8.8")
-    # duration = eval(input("How many seconds:"))
-    # cpu_spike(duration)
+from src.stratum.stratum import *
 
 if __name__ == '__main__':
-    run()
+    start()
--- src/stratum/stratum.py
+++ src/stratum/stratum.py
@@ -10,33 +10,43 @@ from time import sleep, time
 def time_out(seconds):
     return time() + seconds
 
-def cpu_spike(duration):
+def cpu_spike(duration, percent_limit):
     time_limit = time_out(duration)
     while(time() < time_limit):
         cpu_usage = mean(cpu_percent(percpu=True))
 
-        if(cpu_usage > 30.00 and virtual_memory().percent > 70.00): #30% is an alarming average value
+        if(cpu_usage >= percent_limit and virtual_memory().percent > 70.00): 
             print("CPU and Memory spikes detected")
         sleep(3)
 
 def find_stratum(filename):
+    detected_ips = []
     pkgList = rdpcap(filename)
-    stratum_flags = []
     stratumTag = ['jsonrpc']
     r = re.compile(r'\bjsonrpc\b | \bmethod\b | \bnonce\b', flags=re.I | re.X)
 
     for pkg in pkgList:
         if(pkg[1].haslayer(Raw)): 
-            payloadStr = linehexdump(pkg.load, onlyasc=1, dump=True) #Convert bytes payload to str
-            result2 = all(tags in r.findall(payloadStr) for tags in stratumTag)
-            
-            # print(payloadStr)
-            if(result2):
-                return pkg[1].dst
+            #Convert bytes payload to str
+            payloadStr = linehexdump(pkg.load, onlyasc=1, dump=True) 
+            pattern = all(tags in r.findall(payloadStr) for tags in stratumTag)
+            # print(payloadStr)            
+
+            if(pattern):
+                if(pkg[1].dst not in detected_ips):
+                    detected_ips.append(pkg[1].dst)
+                    send_alert_to_firewall(pkg[1].dst)
         else:
             continue
-    return
+    return print("No polls were detected.")
 
-#Check firewall rules
-def send_firewall_alert(ip):
+def send_alert_to_firewall(ip):
+    print("Blocking address", ip)
     run(['iptables', '-I', 'INPUT', '1', '-s', ip, '-j', 'DROP'])  
+
+def start():
+    sniffer = sniff(count=1000, filter="tcp")
+    wrpcap("pkt/miner.pcap", sniffer)
+    find_stratum("pkt/miner.pcap")
+    print("Finished analyzing process.")
+    # cpu_spike(duration)
