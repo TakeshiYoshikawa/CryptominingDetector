e6ad99df93e5247f96c99b00154e55ec1accbffd
Arthur Takeshi <takeshiy096@gmail.com>
2021-01-13 17:47:43 -0400

    Fix pcap file test

--- test/test_stratum.py
+++ test/test_stratum.py
@@ -2,4 +2,4 @@ import pytest
 from src.stratum.stratum import *
 
 def test_pcap_created():
-    assert create_pcap("test",10) == "pkt/test"
+    assert(create_pcap("test.pcap",10) == "pkt/test.pcap")
