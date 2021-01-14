ad607a1effc490d21c1c41f57884c133fe9486e7
Arthur Takeshi <takeshiy096@gmail.com>
2021-01-13 20:16:59 -0400

    Add time out test

--- test/test_stratum.py
+++ test/test_stratum.py
@@ -3,3 +3,6 @@ from src.stratum.stratum import *
 
 def test_pcap_created():
     assert(create_pcap("test.pcap",10) == "pkt/test.pcap")
+
+def test_time_out_with_3_seconds():
+    assert(time_out(3) == round(time() + 3))
