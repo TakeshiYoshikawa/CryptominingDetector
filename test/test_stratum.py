c930b0996d82831f22443a7cc6a1c401d1b7a414
Arthur Takeshi <takeshiy096@gmail.com>
2021-01-13 20:17:29 -0400

    Add cpu spike test

--- test/test_stratum.py
+++ test/test_stratum.py
@@ -6,3 +6,9 @@ def test_pcap_created():
 
 def test_time_out_with_3_seconds():
     assert(time_out(3) == round(time() + 3))
+
+def test_cpu_spike_with_low_percent():
+    assert (isCpuSpike(3, 10, 20) == False)
+
+def test_cpu_spike_with_high_percent():
+    assert (isCpuSpike(3, 40, 20) == True)
