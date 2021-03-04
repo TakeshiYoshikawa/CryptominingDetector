71666e72e198e05f7e55cef235c362b0d2f285f9
Arthur Takeshi <takeshiy096@gmail.com>
2021-03-04 10:25:57 -0400

    Remove comments

--- src/hash_finder.py
+++ src/hash_finder.py
@@ -3,19 +3,6 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-# def this_hash_exist(hash):
-#     URL = "https://localmonero.co/blocks/api/get_block_header/" + hash
-#     while(True):
-#         page = requests.get(URL)
-#         if(page.status_code == 200):
-#             soup = BeautifulSoup(page.content, 'html.parser')
-
-#             r = re.compile(r'"hash":"\b[A-Fa-f0-9]{64}\b"') #Regex to find all SHA256 hashes
-                
-#             return r.findall(str(soup))
-#             #print(r.findall(str(soup))) #Find all hashes in requested page
-#         continue
-
 def find_hash(hash):
     URL = "https://localmonero.co/blocks/api/get_block_header/" + hash
     page = requests.get(URL)
