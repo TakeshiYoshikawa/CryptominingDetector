a1785299c4024b5dd79cb19f6c5ef7010222114c
Arthur Takeshi <takeshiy096@gmail.com>
2021-03-04 10:27:09 -0400

    Remove unused imports

--- src/hash_finder.py
+++ src/hash_finder.py
@@ -1,7 +1,4 @@
-from bs4.element import SoupStrainer
 import requests
-import re
-from bs4 import BeautifulSoup
 
 def find_hash(hash):
     URL = "https://localmonero.co/blocks/api/get_block_header/" + hash
