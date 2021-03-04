263f0e22ea6e6833c3d359bf6cf3e79ff02711ca
Arthur Takeshi <takeshiy096@gmail.com>
2021-03-04 16:00:51 -0400

    Change cpu spike function's name

--- src/cpu_checker.py
+++ src/cpu_checker.py
@@ -5,7 +5,7 @@ from psutil import cpu_percent, virtual_memory
 def time_out(seconds):
     return round(time() + seconds)
 
-def isCpuSpike(duration, cpu_usage, percent_limit):
+def is_cpu_spike(duration, cpu_usage, percent_limit):
     limit_time = time_out(duration)
     while(limit_time > time()):
         # cpu_usage = mean(cpu_percent(interval=1, percpu=True))
