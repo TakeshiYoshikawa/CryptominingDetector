8b71d75259bdd963edef32980f840ad891f1c86a
Arthur Takeshi <takeshiy096@gmail.com>
2021-03-04 22:54:14 -0400

    Add hash finder function to crypto block

--- src/crypto_block.py
+++ src/crypto_block.py
@@ -1,27 +1,37 @@
 import re
-from scapy.all import *
+from scapy.all import sniff, Raw, linehexdump
 from collections import *
 from subprocess import run
+from src.hash_checker import is_mining_block
+
 
 def block():
     detected_ips = []
     
     while(True):
         packet_list = sniff(timeout=1, filter="tcp")
-        stratum_tag = ['jsonrpc']
-        r = re.compile(r'\bjsonrpc\b | \bjob\b | \bblob\b', flags=re.I | re.X)
+        stratum_headers = ['jsonrpc']
+        
+        search = re.compile(r'\bjsonrpc\b | \bjob\b | \bblob\b', flags=re.I | re.X)
+        hash_search = re.compile(r'\b[A-Fa-f0-9]{64}\b')
 
         for packet in packet_list:
             if(packet[1].haslayer(Raw)): 
                 payload_str = linehexdump(packet.load, onlyasc=1, dump=True) #Convert bytes payload to str
-                pattern = all(tags in r.findall(payload_str) for tags in stratum_tag)
-                # print(payloadStr)
+                pattern = all(tags in search.findall(payload_str) for tags in stratum_headers) #Checks if all keywords have been encountered
+                
+                hash = hash_search.findall(payload_str)
 
+                if(is_mining_block(hash) == True):
+                    print("New block detected: {}".format(hash))
+                
+                '''
                 if(pattern):
                     if(packet[1].dst not in detected_ips):
                         store_miner_ip(packet[1].dst)
                         detected_ips.append(packet[1].dst)
                         send_alert_to_firewall(packet[1].dst)
+                '''
             else:
                 continue
             
