5d9ff109b9227b59715623bd0f33ad3ccbd2123e
Arthur Takeshi <takeshiy096@gmail.com>
2021-03-04 22:25:13 -0400

    Fix mining block for empty inputs

--- src/hash_checker.py
+++ src/hash_checker.py
@@ -2,11 +2,14 @@ import requests
 import re
 
 def is_mining_block(hash):
-    URL = "https://localmonero.co/blocks/api/get_block_header/" + hash
-    page = requests.get(URL)
-    response = str(page.json())
+    if(hash != []):
+        URL = "https://localmonero.co/blocks/api/get_block_header/" + hash[0]
+        page = requests.get(URL)
+        response = str(page.json())
 
-    search = re.compile(r'OK')
-    status_block = search.findall(response)
+        search = re.compile(r'OK')
+        status_block = search.findall(response)
 
-    return status_block == ['OK']
+        return status_block == ['OK']
+    else:
+        return False
