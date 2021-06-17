c063703d11c536738a9e4028c5c1b76184bdfb53
TakeshiYoshikawa <takeshiy096@gmail.com>
2021-06-16 22:53:41 -0300

    Fix hash[0] to hash

--- src/hash_checker.py
+++ src/hash_checker.py
@@ -3,10 +3,10 @@ import re
 
 def is_mining_block(hash):
     if(hash != []):
-        URL = "https://localmonero.co/blocks/api/get_block_header/" + hash[0]
+        URL = "https://localmonero.co/blocks/api/get_block_header/" + hash
         page = requests.get(URL)
         response = str(page.json())
-
+        print(response)
         search = re.compile(r'OK')
         status_block = search.findall(response)
 
