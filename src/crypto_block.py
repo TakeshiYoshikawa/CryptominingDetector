55de61c33afe06065ddc328e7a5ff5fcce59347c
TakeshiYoshikawa <takeshiy096@gmail.com>
2021-06-17 02:06:06 -0300

    Refactor hash search

--- src/crypto_block.py
+++ src/crypto_block.py
@@ -1,4 +1,5 @@
 import re
+from time import sleep
 from scapy.all import sniff, Raw, linehexdump
 from collections import *
 from subprocess import run
@@ -6,22 +7,31 @@ from src.hash_checker import is_mining_block
 
 def block():    
     while(True):
+        sleep(1)
         packet_list = sniff(timeout=1, filter="tcp")
-        hash_search = re.compile(r'\b[A-Fa-f0-9]{64}\b')
 
         for packet in packet_list:
             if(packet[1].haslayer(Raw)): 
                 #Convert bytes payload to str
                 payload_str = linehexdump(packet.load, onlyasc=1, dump=True) 
                 
-                hash = hash_search.findall(payload_str)
-                if(is_mining_block(hash) == True):
-                    print("New block detected: {}".format(hash))
-                    print("Block address: {}".format(packet[1].src))
-                    send_alert_to_firewall(packet[1].src)
+                _hash = hash_search(payload_str)
+                if(is_mining_block(_hash) == True):
+                    print("Block Header: {}".format(_hash))
+                    print("Server address: {}".format(packet[1].src))
+                    # send_alert_to_firewall(packet[1].src)
             else:
                 continue
-            
+
+def hash_search(payload) -> str:
+    hash_search = re.compile(r'\b[A-Fa-f0-9]{64}\b')
+    _hash = hash_search.findall(payload)
+
+    # Hash not found
+    if(len(_hash) == 0): 
+        return ""
+    return str(_hash[0])
+
 def send_alert_to_firewall(ip):
     print("Blocking address", ip)
     run(['iptables', '-I', 'INPUT', '1', '-s', ip, '-j', 'DROP'])  
--- src/hash_checker.py
+++ src/hash_checker.py
@@ -1,12 +1,12 @@
 import requests
 import re
 
-def is_mining_block(hash):
-    if(hash != []):
-        URL = "https://localmonero.co/blocks/api/get_block_header/" + hash
+def is_mining_block(_hash) -> bool:
+    if(_hash != ""):
+        URL = "https://localmonero.co/blocks/api/get_block_header/" + _hash
         page = requests.get(URL)
         response = str(page.json())
-        print(response)
+
         search = re.compile(r'OK')
         status_block = search.findall(response)
 
